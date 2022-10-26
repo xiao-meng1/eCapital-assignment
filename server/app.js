4d8c56b9e31570016971fa4430edab23eb76198f
Xiao <xmeng1@ualberta.ca>
2022-10-26 10:41:08 -0600

    Set up logging and error handling

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
+
 var express = require('express');
+const createError = require('http-errors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,8 +17,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_ORIGIN,
+  })
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err,
+  });
+});
 
 module.exports = app;
